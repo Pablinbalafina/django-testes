b6b39831b145cda8c814edd8018071fe99036b3c
Pablinbalafina <lionban667@gmail.com>
2024-12-17 20:42:07 -0300

    rocket

--- meu_projeto/minha_app/urls.py
+++ meu_projeto/minha_app/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contatos/', views.contatos, name='contatos'),
-    path('sobre/', views.sobre, name='sobre')
+    path('sobre/', views.sobre, name='sobre'),
+    path('endereço/', views.endereco, name='endereço'),
+    path('cadastro_cliente/', views.cadastro_cliente, name='cadastro_cliente'),
+    path('cadastro_produto/', views.cadastro_produto, name='cadastro_produto'),
 ]
--- meu_projeto/minha_app/views.py
+++ meu_projeto/minha_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -9,3 +9,25 @@ def contatos(request):
 
 def sobre(request):
     return render(request, 'sobre.html')
+
+def endereco(request):
+    return render (request, 'endereco.html')
+
+def cadastro_cliente(request):
+    if request.method == 'post':
+        nome = request.POST['nome']
+        telefone = request.POST['telefone']
+        email = request.POST['email']
+        Cliente.objects.crete(nome=nome, telefone=telefone, email=email)
+
+
+    return render(request, 'cadastro_cliente.html')
+
+def cadastro_produto(request):
+    if request.method == 'post':
+        nome = request.POST['nome']
+        quantidade = request.POST['quantidade']
+        descricao = request.POST['descrição']
+        Produto.objects.create(nome=nome, quantidade=quantidade, descricao=descricao)
+
+    return render(request, 'cadastro_produto.html')
